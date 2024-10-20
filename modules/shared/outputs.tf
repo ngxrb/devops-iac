5798b18436a017341face00fae9b3a90bb065c4d
Ricardo Javier Barrios Díaz <ricardojbarrios@gmail.com>
2024-10-20 14:58:02 +0100

    feat(shared): add host_regex

--- modules/shared/outputs.tf
+++ modules/shared/outputs.tf
@@ -1,14 +1,19 @@
 output "k8s_name_regex" {
-  description = "Regex for validating Kubernetes resource names"
+  description = "Regex for validating Kubernetes resource names. Must consist of lower-case letters, numbers, and hyphens, and must start and end with an alphanumeric character."
   value       = var.k8s_name_regex
 }
 
 output "path_regex" {
-  description = "Regex for validating file or directory paths"
+  description = "Regex for validating file or directory paths. Supports both absolute and relative paths, including home directory reference with '~'."
   value       = var.path_regex
 }
 
 output "version_regex" {
-  description = "Regex for validating semantic version strings"
+  description = "Regex for validating semantic version strings. Allows versions with comparison operators like '>=', '<=', and versions formatted as X.Y.Z."
   value       = var.version_regex
+}
+
+output "host_regex" {
+  description = "Regex for validating hostnames according to DNS standards (RFC 1035). Ensures valid domain names consisting of alphanumeric characters, hyphens, and dots."
+  value       = var.host_regex
 }
--- modules/shared/variables.tf
+++ modules/shared/variables.tf
@@ -1,17 +1,23 @@
 variable "k8s_name_regex" {
-  description = "Regex for validating Kubernetes resource names"
+  description = "Regex for validating Kubernetes resource names. Must consist of lower-case letters, numbers, and hyphens, and must start and end with an alphanumeric character."
   type        = string
   default     = "^[a-z0-9]([a-z0-9-]{0,61}[a-z0-9])?$"
 }
 
 variable "path_regex" {
-  description = "Regex for validating file or directory paths"
+  description = "Regex for validating file or directory paths. Supports both absolute and relative paths, including home directory reference with '~'."
   type        = string
   default     = "^((~|/)?([^/ ]+(/[^/ ]*)*)?|(.+/)?[^/ ]+)$"
 }
 
 variable "version_regex" {
-  description = "Regex for validating semantic version strings"
+  description = "Regex for validating semantic version strings. Allows versions with comparison operators like '>=', '<=', and versions formatted as X.Y.Z."
   type        = string
   default     = "^(>=|<=|>|<|=|~>)?\\s*\\d+\\.\\d+\\.\\d+$"
+}
+
+variable "host_regex" {
+  description = "Regex for validating hostnames according to DNS standards (RFC 1035). Ensures valid domain names consisting of alphanumeric characters, hyphens, and dots."
+  type        = string
+  default     = "^([a-zA-Z0-9]([-a-zA-Z0-9]*[a-zA-Z0-9])?\\.)*[a-zA-Z0-9]([-a-zA-Z0-9]*[a-zA-Z0-9])?$"
 }
